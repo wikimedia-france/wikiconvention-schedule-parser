cda7d0d0e0ba4f994c2434e5cca68f1cfecf4e37
Wikimedia France <wikimedia@toolbox.wikimedia.fr>
2018-09-26 19:57:10 +0200

    2018 update

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@ ROOTDIR="$(readlink -f $(dirname $0))"
 
 
 APP="/var/www/wikiconvention/wikiconvention-schedule-app"
-META="https://meta.wikimedia.org/w/api.php?action=parse&format=json&text=%7B%7B%3AWikiConvention+francophone%2F2017%2FProgramme%7Cshow%3DSimple%7D%7D&prop=text&contentmodel=wikitext"
+META="https://meta.wikimedia.org/w/api.php?action=parse&format=json&text=%7B%7B%3AWikiConvention+francophone%2F2018%2FProgramme%7Cshow%3DSimple%7D%7D&prop=text&contentmodel=wikitext"
 
 TMP="${ROOTDIR}/tmp/json"
 
